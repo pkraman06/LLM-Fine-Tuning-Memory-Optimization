6ca690adca51dddccdf98c58f32bc6f6c2666213
KOTHANDA RAMAN P <ramantech06@gmail.com>
2026-07-28 23:00:10 +0530

    Add Hugging Face token login to launch script

--- launch_fsdp.sh
+++ launch_fsdp.sh
@@ -6,6 +6,10 @@ MODEL_NAME=${MODEL_NAME:-"meta-llama/Llama-2-7b-hf"}
 TRAIN_JSON=${TRAIN_JSON:-"./cuda.json"}
 OUTPUT_DIR=${OUTPUT_DIR:-"./checkpoints/cuad-qlora-fsdp"}
 
+if [ -n "${HF_TOKEN:-}" ]; then
+  huggingface-cli login --token "${HF_TOKEN}" --add-to-git-credential
+fi
+
 accelerate launch --config_file configs/fsdp_config.yaml src/train.py \
   --model_name "${MODEL_NAME}" \
   --train_json "${TRAIN_JSON}" \
@@ -15,3 +19,4 @@ accelerate launch --config_file configs/fsdp_config.yaml src/train.py \
   --learning_rate 2e-4 \
   --num_train_epochs 3 \
   --bf16
+
